0639ad1a0c20569e1ede81778d792a2b010e59ef
Joel Ward <jmward@gmail.com>
2019-11-28 10:06:18 -0800

    update

--- src/reader.lisp
+++ src/reader.lisp
@@ -20,6 +20,16 @@
   (if (eq   (peekc s)  c) (eat-c s)
       (cons (readx s   c) (rdl c s))))
 
+(defvar *indent* 0)
+
+(defun read-nl (s &optional c)
+  (declare (ignore c))
+  (setf *indent* (length (read-wc s)))
+  (if (eq (peekc s) NL) (n (readc   s)
+			   (read-nl s))
+      (n (f. "new indent is ~a" *indent*)
+	 (values))))
+
 (defun xrnl (s)
   (read-wc s)
   (if (member (peekc s) (list #\¶ NL nil))  nil
@@ -38,46 +48,65 @@
 	    (list (:1 x) (:0 x))
 	    (list (:0 x) (:1 x))))
     (3  (list (:1 x) (:0 x) (:2 x)))
-    (4  (if (eq (:1 x) (:3 x)) (error "X"))
+    (4  (if (eq (:1 x) (:3 x)) t
+	    (error "X"))
      t  (list (:1 x) (:0 x) (:2 x)))
-    (5  (if (eq (:1 x) (:3 x)) (error "X"))
+    (5  (if (eq (:1 x) (:3 x)) t
+	    (error "X"))
      t  (list (:1 x) (:0 x) (:2 x) (:4 x)))
-    (6  (if (eq (:1 x) (:3 x))
-	    (if (eq (:1 x) (:5 x)) (error "X")))
+    (6  (if (eq (:1 x) (:3 x)) t
+	    (if (eq (:1 x) (:5 x)) t
+		(error "X")))
      t  (list (:1 x) (:0 x) (:2 x) (:4 x)))
-    (7  (if (eq (:1 x) (:3 x))
-	    (if (eq (:1 x) (:5 x)) (error "X")))
+    (7  (if (eq (:1 x) (:3 x)) t
+	    (if (eq (:1 x) (:5 x)) t
+		(error "X")))
      t  (list (:1 x) (:0 x) (:2 x) (:4 x) (:6 x)))
-    (8  (if (eq (:1 x) (:3 x))
-	    (if (eq (:1 x) (:5 x))
-		(if (eq (:1 x) (:7 x)) (error "X"))))
+    (8  (if (eq (:1 x) (:3 x)) t
+	    (if (eq (:1 x) (:5 x)) t
+		(if (eq (:1 x) (:7 x)) t
+		    (error "X"))))
      t  (list (:1 x) (:0 x) (:2 x) (:4 x) (:6 x)))
-    (9  (if (eq (:1 x) (:3 x))
-	    (if (eq (:1 x) (:5 x))
-		(if (eq (:1 x) (:7 x)) (error "X"))))
+    (9  (if (eq (:1 x) (:3 x)) t
+	    (if (eq (:1 x) (:5 x)) t
+		(if (eq (:1 x) (:7 x)) t
+		    (error "X"))))
      t  (list (:1 x) (:0 x) (:2 x) (:4 x) (:6 x) (:8 x)))
-    (10 (if (eq (:1 x) (:3 x))
-	    (if (eq (:1 x) (:5 x))
-		(if (eq (:1 x) (:7 x))
-		    (if (eq (:1 x) (:9 x)) (error "X")))))
+    (10 (if (eq (:1 x) (:3 x)) t
+	    (if (eq (:1 x) (:5 x)) t
+		(if (eq (:1 x) (:7 x)) t
+		    (if (eq (:1 x) (:9 x)) t
+			(error "X")))))
      t  (list (:1 x) (:0 x) (:2 x) (:4 x) (:6 x) (:8 x)))))
 
+(defun read-lines (_1)
+  (read-nl _1)
+  (cons 'n (loop :with indent0 := (1+ *indent*)
+		 :collect           (rnl     _1)
+		 :do                (read-nl _1)
+		 :until     (member (peekc   _1) '(#\¶ nil))
+		 :until     (    |<| indent0 *indent*)
+		 :do        (if (|>| indent0 *indent*)
+				(error "indent error")))))
+(defun read-keyword (_1)
+  (intern (string-upcase (cs (read-sym _1))) :keyword))
+
+(defun read-semi (_1)
+  (if (eq (peekc _1) #\;) (n (read-line _1) (values)) '|;|))
+
 (defun turn-it-on ()
-  (set-mchar #\; (if (eq (peekc _1) #\;) (n (read-line _1) (values)) '|;|))
+  (set-mchar #\; (read-semi _1))
   (set-mchar #\' (list (quote quote) (readx _1)))
   (set-mchar #\$ (list (quote quote) (readx _1)))
-  (set-mchar NL  (values))
+  (set-macro-character NL 'read-nl)
   (set-mchar CR  (values))
   (set-mchar SP  (values))
   (set-mchar TB  (values))
   (set-mchar FF  (values))
   (set-mchar VT  (values))
-  (set-mchar #\: (cons 'n (loop :initially (read-ws _1)
-				:collect   (rnl     _1)
-				:do        (read-ws _1)
-				:until     (member (peekc _1) '(#\¶ nil)))))
+  (set-mchar #\: (read-lines _1))
   (set-mchar #\¶ (values))
-  (set-mchar #\/ (values (intern (string-upcase (cs (read-sym _1))) "KEYWORD")))
+  (set-mchar #\/ (read-keyword _1))
   (set-mchar #\! (rnl   _1))
   (set-mchar #\~ (readx _1))
   (set-mchar #\, '\,)
@@ -91,6 +120,7 @@
 (defun turn-it-off ()
   (set-mchar #\( (read-delimited-list #\) _1))
   (set-mchar #\) (error ")"))
+  (set-macro-character #\: nil)
   (set-macro-character #\^ nil)
   (set-macro-character #\; nil)
   (set-macro-character #\' nil)
--- tst/test.lisp
+++ tst/test.lisp
@@ -6,16 +6,7 @@
 (if2 1 2)
 (if3 1 2 3)
 (turn-it-on)
-~p.(list((3,45)))
 
-: p. list((3,4559)) ¶
-
-! print ':
-       print 100
-       print /qwert
-       print (10 + 5)
-       ¶
-
-<print <q <q nil>>>
-
+:    print 55
+     ¶
 <quit>
