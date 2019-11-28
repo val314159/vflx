2ff68f1d442ba587f57f6cb89e01cd4b3401303c
Joel Ward <jmward@gmail.com>
2019-11-27 19:33:32 -0800

    update

--- src/reader.lisp
+++ src/reader.lisp
@@ -1,2 +1,18 @@
 (in-package :v)
 
+(defun readx (s &optional c)
+  (let ((x (readr s))
+	(y (peekc s)))
+    (if (not y) x
+	(if (eq y c) x
+	    (let ((m (get-mchar y)))
+	      (if (not m) x
+		  (let ((z (mv-list (c m s (readc s)))))
+		    (if (not z) x
+			(cons x z)))))))))
+
+(defun rdl (c s)
+  (read-ws s)
+  (if (eq (peekc s) c) (eatc s)
+      (cons (readx s c) (rdl c s))))
+
